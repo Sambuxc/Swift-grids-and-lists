04afd9a29865bbdccb61cc80b32b30984d4cceca
Sambuxc <samwbru@gmail.com>
2025-11-14 08:12:54 +0000

    Initial Commit

---
+++ iMushroom/iMushroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMushroomApp.swift
+//  iMushroom
+//
+//  Created by Samuel Bruton on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct iMushroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
