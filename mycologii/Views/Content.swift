56262b3d92d5d871da35b175c138cb01c88da254
Sambuxc <samwbru@gmail.com>
2025-11-14 17:34:47 +0000

    updates mock data content and adds icon

--- mycologii/Views/Content.swift
+++ mycologii/Views/Content.swift
@@ -16,17 +16,11 @@ struct Content: Hashable, Identifiable {
 }
 
 struct MockData {
-    
-    static let sampleFramework = Content(name: "Psilocybe",
-                                           image: "arkit",
-                                           url: "https://developer.apple.com/augmented-reality/arkit",
-                                           desc: "ARKit 4 introduces a brand-new Depth API, creating a new way to access the detailed depth information gathered by the LiDAR Scanner on iPhone 12 Pro, iPhone 12 Pro Max, and iPad Pro.\n\nLocation Anchors leverages the higher-resolution data in Apple Maps to place AR experiences at a specific point in the world in your iPhone and iPad apps. And support for face tracking extends to all devices with the Apple Neural Engine and a front-facing camera, so even more users can experience the joy of AR in photos and videos.")
-    
     static let frameworks = [
-        Content(name: "App Clips",
-                  image: "app-clip",
-                  url: "https://developer.apple.com/app-clips",
-                  desc: "App Clips are a great way for users to quickly access and experience what your app has to offer. An App Clip is a small part of your app that’s discoverable at the moment it’s needed. App Clips are fast and lightweight so a user can open them quickly.\n\nWhether they're ordering take-out from a restaurant, renting a scooter, or setting up a new connected appliance for the first time, users will be able to start and finish an experience from your app in seconds. And when they’re done, you can offer the opportunity to download your full app from the App Store."),
+        Content(name: "Strophariaceae",
+                  image: "stroph-icon",
+                  url: "https://en.wikipedia.org/wiki/Strophariaceae",
+                  desc: "The Strophariaceae are a family of fungi in the order Agaricales. Under an older classification, the family covered 18 genera and 1316 species.[1] The species of Strophariaceae have red-brown to dark brown spore prints, while the spores themselves are smooth and have an apical germ pore. These agarics are also characterized by having a cutis-type pileipellis. Ecologically, all species in this group are saprotrophs, growing on various kinds of decaying organic matter. The family was circumscribed in 1946 by mycologists Rolf Singer and Alexander H. Smith."),
         
         Content(name: "ARKit",
                   image: "arkit",
