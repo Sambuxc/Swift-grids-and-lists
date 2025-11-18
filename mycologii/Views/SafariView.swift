501fcefff3ec2547dc7c00f7d3052e0d6bfcce71
Sambuxc <samwbru@gmail.com>
2025-11-18 20:17:46 +0000

    Adds Safari View Controller

---
+++ mycologii/Views/SafariView.swift
@@ -0,0 +1,23 @@
+//
+//  SafariView.swift
+//  mycologii
+//
+//  Created by Samuel Bruton on 18/11/2025.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {}
+}
+
+#Preview {
+    SafariView(url: URL(string:"http://google.com")!)
+}
