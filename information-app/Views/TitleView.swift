8221ea825eb991bdc24a3359ede0ede6bf2ce167
Sambuxc <samwbru@gmail.com>
2025-11-22 20:08:35 +0000

    adds more spacing using grid mode

--- information-app/Views/TitleView.swift
+++ information-app/Views/TitleView.swift
@@ -9,23 +9,23 @@ import SwiftUI
 struct TitleView: View {
     @StateObject private var appSettingsModel = AppSettingsModel()
     var content: Content
-    
 
     var body: some View {
         let size: CGFloat = appSettingsModel.isGridMode ? 90 : 60
         
         if appSettingsModel.isGridMode {
             VStack(alignment: .center, spacing: 0) {
-                ContentView(content: content, size: size)
+                ContentView(isGridMode: appSettingsModel.isGridMode, content: content, size: size)
             }
             .padding()
         } else {
-            ContentView(content: content, size: size)
+            ContentView(isGridMode: appSettingsModel.isGridMode, content: content, size: size)
         }
     }
 }
 
 struct ContentView: View {
+    let isGridMode: Bool
     let content: Content
     let size: CGFloat
     
@@ -34,6 +34,7 @@ struct ContentView: View {
             .resizable()
             .frame(width: size, height: size)
             .aspectRatio(contentMode: .fit)
+            .padding(.trailing, isGridMode ? 0 : 16)
         Text(content.name)
             .font(.title)
             .scaledToFit()
