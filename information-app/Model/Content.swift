0196358057ac0a45e606dfd27b43c013a2bfe5b4
Sambuxc <samwbru@gmail.com>
2025-11-24 17:42:48 +0000

    updates sample content

--- information-app/Model/Content.swift
+++ information-app/Model/Content.swift
@@ -140,8 +140,8 @@ struct MockData {
     
     static let sampleContent = Content(
         name: "Sample Content",
-        image: "placeholder",
-        url: "google.com/search?q=could+not+find",
-        desc: "Sample content because data is unavailable."
+        image: "app-clip",
+        url: "https://www.apple.com/404",
+        desc: "Easily build widgets and make them available across iOS, iPadOS, and macOS using the WidgetKit framework and the new widget API for SwiftUI. Widgets now come in multiple sizes, and users can visit the new widget gallery to search, preview sizes, and place them anywhere on the Home screen to access important details at a glance.\n\nThey can also add Smart Stacks — sets of widgets that use on-device intelligence to surface the right widget at the right moment, based on factors like time, location, and activity."
     )
 }
